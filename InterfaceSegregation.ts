9ee0cd6677fe4cab189920020888ad6607eee692
Leonardo Micheloni <leonardo.micheloni@tokiota.com>
2019-03-26 17:00:23 +0100

    typo

--- InterfaceSegregation.ts
+++ InterfaceSegregation.ts
@@ -1,10 +1,10 @@
 import { SOLIDPrinciple } from "./SOLIDPrinciple";
-import { Biciclete } from "./Interface segregation/Biciclete";
+import { Bicycle } from "./Interface segregation/Bicycle";
 import { Vehicle } from "./Interface segregation/Vehicle";
 
 export class InterfaceSegregation implements SOLIDPrinciple{
     Run() {
-        var vehicle : Vehicle = new Biciclete();
+        var vehicle : Vehicle = new Bicycle();
         vehicle.takeOff();
     }
 }
